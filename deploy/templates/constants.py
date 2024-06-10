1e4a9a946b5fec6a51b593ee580e3cc71a42a40a
Arnau Gabriel Atienza <arnau12321@gmail.com>
2024-06-10 11:58:29 +0200

    Update deploy constants

--- deploy/templates/constants.py
+++ deploy/templates/constants.py
@@ -1,9 +1,22 @@
-MILVUS_HOST = "{{ hostvars[groups['milvus'][0]].private_ip }}"
+# Milvus config
+MILVUS_HOST = "localhost"
 MILVUS_PORT = "19530"
 MILVUS_NAMESPACE = "default"
 
-PRAVEGA_CONTROLLER = "tcp://{{ hostvars[groups['bookkeeper'][0]].private_ip }}:9090"
+# Pravega config
+PRAVEGA_CONTROLLER = "172.28.1.1:9090"
 PRAVEGA_SCOPE = "examples"
+
+# Inference Logs
+DO_LATENCY_LOG = True
+DO_BATCH_LOG = False
+
+# Path for all logs
+LOG_PATH = "/project/results"
+RESULT_PATH = "/project/results"
+
+# Query params
+QUERY_ACCURACY = 0.9
 
 if __name__ == "__main__":
     """Print the environment variables for the shell scripts"""
