3e373a660cf6b8ea3bc92c3bb9a26e46cce545fc
Gerard Finol <gerard.finol@urv.cat>
2024-06-17 12:39:44 +0200

    fix benchmark

--- benchmarks/encoder_decoder_experiments/ingestion_default.sh
+++ benchmarks/encoder_decoder_experiments/ingestion_default.sh
@@ -31,13 +31,9 @@ KEY_FRAME_INTERVAL=$((1*$FPS))
 
 gst-launch-1.0 \
 -v \
-filesrc location="${FILESRC_PATH}" \
-! qtdemux \
-! h264parse \
-! avdec_h264 \
+videotestsrc name=src is-live=true do-timestamp=true num-buffers=$(($SIZE_SEC*$FPS)) \
+! "video/x-raw,format=RGB,width=800,height=600,framerate=${FPS}/1" \
 ! videoconvert \
-! videoscale \
-! video/x-raw,width=940,height=560 \
 ! x264enc key-int-max=${KEY_FRAME_INTERVAL} \
 ! timestampcvt input-timestamp-mode=start-at-current-time \
 ! pravegasink stream=${PRAVEGA_SCOPE}/${PRAVEGA_STREAM} controller=${PRAVEGA_CONTROLLER} allow-create-scope=true seal=true sync=false timestamp-mode=tai buffer-size=1024
